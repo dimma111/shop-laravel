2a07a9cc9f561b9590cf0aca5fef0e5e0d198ea8
Dmitriy <marinenko96@yandex.ru>
2023-05-23 23:55:56 +0500

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('/catalog', function() {
+    return view('catalog');
+});
+
+Route::get('/product', function() {
+    return view('product');
 });
